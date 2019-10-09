dc4d7468e659489eed7e46543c8a87649e6c2c51
kmk935-prog <kmk935.prog@gmail.com>
2019-10-09 20:00:24 +0900

    add to classViews
    add to HelloViews

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 from .forms import HelloForm
 
 # Create your views here.
-def index(request):
-    params = {
-        'title':'Hello',
-        'message':'your data:',
-        'form':HelloForm(),
-    }
-    if (request.method == 'POST'):
-        params['message'] = '名前：' + request.POST['name'] + '<br>メール：' + request.POST['mail'] + '<br>年齢：' + request.POST['age']
-        params['form'] = HelloForm(request.POST)
-    return render(request, 'hello/index.html', params)
+class HelloView(TemplateView):
+
+    def __init__(self):
+        self.params = {
+            'title':'Hello',
+            'form':HelloForm(),
+            'result':None
+        }
+    
+    def get(self, request):
+        return render(request,'hello/index.html',self.params)
+    
+    def post(self, request):
+        if ('check' in request.POST):
+            self.params['result'] = 'Checked!!'
+        else:
+            self.params['result'] = 'not checked...'
+        self.params['form'] = HelloForm(request.POST)
+        return render(request,'hello/index.html',self.params)
